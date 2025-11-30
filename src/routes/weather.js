3237b93c558242b1f600c1fd2f58d27effbacdb6
nicoleflourish <nicole@flourishgardensolutions.com>
2025-11-30 16:30:08 -0600

    revert to earlier weather route

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getWeatherData } = require('../controllers/weatherController');
-const { cacheMiddleware } = require('../middleware/cache');
+const weatherController = require('../controllers/weatherController');
 
-// Cache weather data for 6 hours (21600000 ms)
-// Weather patterns don't change frequently, so this is safe
-// Each zip code gets its own cache entry
-router.get('/:zipCode', cacheMiddleware(21600000), getWeatherData);
+router.get('/:zipCode', weatherController.getWeatherByZip);
 
 module.exports = router;
